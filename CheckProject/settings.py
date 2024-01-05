fc2c5a3820cb1d3afeefdbab2d6308cad3dd1e33
t4gamer <tahagadban@gmail.com>
2024-01-05 18:25:30 +0200

    moved the secretKey to the settings file it caused me to many problems

--- CheckProject/settings.py
+++ CheckProject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
-from .secret_key import KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KEY
+SECRET_KEY = "django-insecure-eh3=7gva5q29^xd#v3c4z&2#64kks0#bk=856fu4yi!27t(vrc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
