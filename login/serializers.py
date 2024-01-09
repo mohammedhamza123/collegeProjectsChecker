dccc4f6495fd98c0be1acd98a30bcdb249a65d8f
t4gamer <tahagadban@gmail.com>
2024-01-09 19:33:49 +0200

    added groups to the register serializer

--- login/serializers.py
+++ login/serializers.py
@@ -51,6 +51,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             email=validated_data["email"],
             first_name=validated_data["first_name"],
             last_name=validated_data["last_name"],
+            groups=validated_data["groups"]
         )
 
         user.set_password(validated_data["password"])
