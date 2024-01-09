d17a824e7e42c49ecbfdc6baa17e04cb711bf705
t4gamer <tahagadban@gmail.com>
2024-01-09 19:47:03 +0200

    fixed the way groups added on register

--- login/serializers.py
+++ login/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model 
+from django.contrib.auth.models import User ,Group
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
@@ -51,9 +51,10 @@ class RegisterSerializer(serializers.ModelSerializer):
             email=validated_data["email"],
             first_name=validated_data["first_name"],
             last_name=validated_data["last_name"],
-            groups=validated_data["groups"]
         )
 
+        group = Group.objects.get(id=validated_data["groups"][0])
+        user.groups.add(group)
         user.set_password(validated_data["password"])
         user.save()
 
