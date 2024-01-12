44593f69ab2a27c78cd55d943945bc41eeb0d94f
t4gamer <tahagadban@gmail.com>
2024-01-12 23:55:05 +0200

    the admin can request all the channels

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response , get_object_or_404
+from rest_framework.response import Response
 
 from .serializers import *
 from django.contrib.auth.models import User,Group
