f6a6c18b858d80a879710f228b33f4f9c62ee465
t4gamer <tahagadban@gmail.com>
2024-01-13 19:50:57 +0200

    fixed the problem with messages request for a certain channel

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render ,get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 
 from .serializers import *
 from django.contrib.auth.models import User,Group
@@ -18,10 +19,11 @@ class MessegeViewSet(ModelViewSet):
     queryset = Messege.objects.all()
 
     def list(self, request, *args, **kwargs):
-        user = get_object_or_404(User,id=request.user)
+        user = get_object_or_404(User,id=request.user.id)
         user_group = Group.objects.filter(user=user.id).first()
         channel_query = request.query_params.get('channel')
         if channel_query:
+            channel = get_object_or_404(Channel,id=int(channel_query),members=user.id)
             queryset = self.queryset.filter(id=int(channel_query))
         else:
             if user_group:
@@ -31,13 +33,15 @@ class MessegeViewSet(ModelViewSet):
                     student = get_object_or_404(Student,id=user.id)
                     channel = Channel.objects.filter(projcet=student.project)
                     queryset = self.queryset.filter(channel=channel.id)
+            else:
+                raise NotFound(detail="Error 404, page not found", code=404)
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
         serializer = self.get_serializer(queryset, many=True)
-        return Response({"datum", serializer.data})
+        return Response({"datum":serializer.data})
 
 
 class ChannelViewSet(ModelViewSet):
