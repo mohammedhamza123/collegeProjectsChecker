4065471b2c4397ba16aa511f85ed59932037028c
t4gamer <tahagadban@gmail.com>
2024-04-03 03:42:47 +0200

    added id field to the reponse of register endPoint

--- login/serializers.py
+++ login/serializers.py
@@ -24,6 +24,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
+            "id",
             "username",
             "password",
             "password2",
